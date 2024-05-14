5ae90128ad41f59cc2a507aa934cbbf6cc6984c9
TheRednax <136997904+TheRednax@users.noreply.github.com>
2024-05-14 10:53:44 +0200

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Ui.Mvc.Models;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(SignInModel model)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            return View();
+        }
+    }
+}
